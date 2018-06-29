dac8ee4f1baf0a66abc3cdd91dd110c532b300f6
wise4rmgod <wise4rmgod@gmail.com>
2018-06-30 00:43:36 +0100

    Fix the error in ViewEntry Activity,it can can fetch data from firebase

--- app/src/main/java/com/example/multikskills/journal_app/ViewEntryActivity.java
+++ app/src/main/java/com/example/multikskills/journal_app/ViewEntryActivity.java
@@ -1,4 +1,4 @@
-package com.example.multikskills.journal_app;
+ package com.example.multikskills.journal_app;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,9 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
-public class ViewEntryActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+ public class ViewEntryActivity extends AppCompatActivity {
+     TextView date,message,title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +20,23 @@ public class ViewEntryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("journal");
+
+        //initialize edittext and button
+        title =  findViewById(R.id.title);
+        message =  findViewById(R.id.message);
+        date   = findViewById(R.id.date);
+
+//getExtra is used to fetch data passed through intent
+        final String title1 = getIntent().getStringExtra("title");
+        String message1 = getIntent().getStringExtra("message");
+        String date1 = getIntent().getStringExtra("date");
+        final String key = getIntent().getStringExtra("key");
+
+        //show values in Edittexts
+        title.setText(title1);
+        message.setText(message1);
+        date.setText(date1);
     }
 
 }
