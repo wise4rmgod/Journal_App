83469f58ef521324decaf4d893ceb312aed734ea
wise4rmgod <wise4rmgod@gmail.com>
2018-07-01 15:17:56 +0100

    add Viewentryactivity presenter and viewentryMVP

--- app/src/main/java/com/example/multikskills/journal_app/Presenter/Viewentryactivitypresenter.java
+++ app/src/main/java/com/example/multikskills/journal_app/Presenter/Viewentryactivitypresenter.java
@@ -1,4 +1,12 @@
 package com.example.multikskills.journal_app.Presenter;
 
+import com.example.multikskills.journal_app.ViewentryMVP;
+
 public class Viewentryactivitypresenter {
+    private final ViewentryMVP.view view;
+
+    public Viewentryactivitypresenter(ViewentryMVP.view view){
+        this.view=view;
+    }
+
 }
--- app/src/main/java/com/example/multikskills/journal_app/ViewentryMVP.java
+++ app/src/main/java/com/example/multikskills/journal_app/ViewentryMVP.java
@@ -1,4 +1,10 @@
 package com.example.multikskills.journal_app;
 
 public interface ViewentryMVP {
+    interface  view{
+        void showsign();
+    }
+    interface presenter{
+        void buttonclicksigin();
+    }
 }
